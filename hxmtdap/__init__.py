a8f96ebd7eecbfffa39a87c4f93782639fb6b02e
Chenxu Gao <cxgao@shao.ac.cn>
2024-11-07 15:52:43 +0800

    add LICENSE

--- hxmtdap/__init__.py
+++ hxmtdap/__init__.py
@@ -7,6 +7,6 @@ from .tools import lcutils
 from .tools import pdsutils
 from .tools import specutils
 
-__version__ = "0.2.1"
+__version__ = "0.2.4"
 
 __all__ = ["LEDA", "MEDA", "HEDA", "utils", "lcutils", "pdsutils", "specutils"]
